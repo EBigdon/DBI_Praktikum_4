57b3197a01c42cddc3d75359c1cecb8e7d38eeaa
Henrik <henrik2koo@gmail.com>
2020-12-08 11:58:38 -0800

    lirum larum

--- src/com/company/Fill_Database.java
+++ src/com/company/Fill_Database.java
@@ -23,52 +23,53 @@ public class Fill_Database {
                 "RSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJK";
         int counter = 0;
 
-        for (int i = 0; i < n; i++) {
-            //Branch
-            for (int j = 0; j < 1; j++) {
-                String name = Integer.toString(i) + Integer.toString(j) + fillString.substring(0, 20 - int_string_length(i, j));
+
+            for (int j = 0; j < 1 * n; j++) {
+                String name = Integer.toString(j) + fillString.substring(0, 20 - int_string_length(j));
                 int branchId = counter;
                 int balance = 0;
-                String address = Integer.toString(i) + Integer.toString(j) + fillString.substring(0, 72 - int_string_length(i, j));
+                String address = Integer.toString(j) + fillString.substring(0, 72 - int_string_length(j));
                 String querybranch = "INSERT INTO `branches` (`branchid`, `branchname`, `balance`, `address`) VALUES (" + branchId + ", '" + name + "'," + balance + ", '" + address + "')";
                 myDatabase.executeQuerry(querybranch);
                 counter++;
             }
-            /*
+
             counter = 0;
             //Accounts
-            for (int j = 0;j<n;j++) {
-                String name = Integer.toString(i) + Integer.toString(j) + fillString.substring(0, 20 - int_string_length(i, j));
+            for (int j = 0;j<100000 * n;j++) {
+                String name = Integer.toString(j) + fillString.substring(0, 20 - int_string_length(j));
                 int accId = counter;
                 int balance = 0;
                 int branchId = counter;
-                String address = Integer.toString(i) + Integer.toString(j) + fillString.substring(0,68 - int_string_length(i, j));
-                String querybranch = "INSERT INTO branch ("+ accId + "," + name + "," + balance + "," + address + ")" ;
-
+                String address = Integer.toString(j) + fillString.substring(0,68 - int_string_length(j));
+                String querybranch = "INSERT INTO `accounts` (`accid`, `name`, `balance`, `branchid`, `address`) VALUES (" + accId + ", '" + name + "'," + balance + ", '" + branchId + ", '"+ address + "')";
+                myDatabase.executeQuerry(querybranch);
                 counter++;
             }
             counter = 0;
             //tellers
-            for (int j = 0;j<n;j++) {
+            for (int j = 0;j< 10 * n;j++) {
                 int tellerid = counter;
-                String tname = Integer.toString(i) + Integer.toString(j) + fillString.substring(0, 20 - int_string_length(i, j));
+                String tname = Integer.toString(j) + fillString.substring(0, 20 - int_string_length(j));
                 int balance = 0;
                 int branchid = counter;
-                String address = Integer.toString(i) + Integer.toString(j) + fillString.substring(0,68 - int_string_length(i, j));
+                String address = Integer.toString(j) + fillString.substring(0,68 - int_string_length(j));
+                String querybranch = "INSERT INTO `tellers` (`tellerid`, `tellername`, `balance`, `branchid`,`address`) VALUES (" + tellerid + ", '" + tname + "'," + balance + "',"+ branchid + ", '" + address + "')";
+                myDatabase.executeQuerry(querybranch);
                 counter++;
             }
-            counter = 0;
+            /*counter = 0;
             //history
             for (int j = 0;j<n;j++) {
 
-            }
+            }*/
 
-             */
-        }
+
+
     }
 
-    public static int int_string_length(int i, int j) {
-        return (Integer.toString(i).length() + Integer.toString(j).length());
+    public static int int_string_length(int i) {
+        return Integer.toString(i).length();
     }
 }
 
