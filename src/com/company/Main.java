da98c229f879171d210f0bb2a4aa51545b3e5fd9
Henrik <henrik2koo@gmail.com>
2020-12-09 09:17:17 -0800

    String länge und counter removed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,6 +26,7 @@ public class Main {
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
         System.out.println("Laufzeit in ms: " + timeElapsed);
+        
     }
 
 }
