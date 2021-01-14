a0ee59819abf6fabb0055d54cbae5145dda8a03c
Erik <erik@bigdon.de>
2021-01-13 16:05:02 -0800

    BalanceTx
    Working balance transaction as stored procedure

--- src/com/company/StoredProceduresManager.java
+++ src/com/company/StoredProceduresManager.java
@@ -11,12 +11,11 @@ public class StoredProceduresManager {
     public StoredProceduresManager(Connection conn) {
         this.conn = conn;
         testProcedure();
-        System.out.println("\n\n\n\n");
         analyseProcedure(100);
+        balanceProcedure(519847);
     }
 
     public static void testProcedure() {
-        //SELECT branchid, balance FROM `branches` WHERE balance < 175000 ORDER BY `balance`  ASC
         try {
             CallableStatement stmt = conn.prepareCall("{call testProcedure(175000)}");
             Boolean hasResult = stmt.execute();
@@ -32,23 +31,42 @@ public class StoredProceduresManager {
 
     }
 
-    public static void analyseProcedure(final int depositAmount) {
+    public static int balanceProcedure(final int accountid) {
+        try {
+            CallableStatement stmt = conn.prepareCall("{call balanceProcedure(" + accountid + ")}");
+            Boolean hasResult = stmt.execute();
+            if (hasResult) {
+                ResultSet res = stmt.getResultSet();
+                while (res.next()) {
+                    return res.getInt("balance");
+                }
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public static int analyseProcedure(final int depositAmount) {
         try {
             CallableStatement stmt = conn.prepareCall("{call analyseProcedure(" + depositAmount + " )}");
             Boolean hasResult = stmt.execute();
             if (hasResult) {
                 ResultSet res = stmt.getResultSet();
                 while (res.next()) {
-                    System.out.println(res.getInt("Anz"));
+                    return res.getInt("Anz");
                 }
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+        return 0;
     }
 
     public static void StringsToCreateProcedures() {
-        String test = "CREATE PROCEDURE `testProcedure`(IN `balanceToCheck` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT branchid, balance FROM `branches` WHERE balance < balanceToCheck ORDER BY `balance` ASC";
-        String analyseProcedure = "CREATE PROCEDURE `analyseProcedure`(IN `depositAmount` INT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT Count(accid) as Anz FROM history WHERE delta = depositAmount GROUP BY delta; ";
+        String test = "CREATE PROCEDURE `testProcedure`(IN `balanceToCheck` INT) COMMENT 'procedure to test stored procedures' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER Begin SELECT branchid, balance FROM `branches` WHERE balance < balanceToCheck ORDER BY `balance` ASC; END";
+        String analyse = "CREATE PROCEDURE `analyseProcedure`(IN `depositAmount` INT) COMMENT 'The analyse transaction' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT Count(accid) as Anz FROM history WHERE delta = depositAmount GROUP BY delta; END";
+        String balance = "CREATE PROCEDURE `balanceProcedure`(IN `accountid` INT) COMMENT 'represents the balance transaction' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT balance FROM accounts WHERE accid = accountid; END";
     }
 }
