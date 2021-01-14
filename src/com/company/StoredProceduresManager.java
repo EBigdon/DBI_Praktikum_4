630b2abd57dba06d2ea1b70ab679307a116ff408
Erik <erik@bigdon.de>
2021-01-14 01:21:14 -0800

    checkstyle
    used checkstyle for stored procedures manager.

--- src/com/company/StoredProceduresManager.java
+++ src/com/company/StoredProceduresManager.java
@@ -6,36 +6,58 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Utility Class to call stored procedures.
+ */
 public class StoredProceduresManager {
+    /**
+     * Connection to database to use.
+     */
     private static Connection conn;
 
-    public StoredProceduresManager(Connection conn) {
-        this.conn = conn;
+    /**
+     * Constructor of Class.
+     *
+     * @param connection Connection to set.
+     */
+    public StoredProceduresManager(final Connection connection) {
+        conn = connection;
         analyseProcedure(100);
         balanceProcedure(519847);
-        depositProcedure(519847, 111, 90
-                , 200);
+        depositProcedure(519847, 111, 90, 200);
     }
 
-    public static int depositProcedure(final int accid, final int tellerid,
-                                       final int branchid, final int depositAmount) {
+    /**
+     * Methode to call the deposit procedure.
+     *
+     * @param accid         the account id
+     * @param tellerid      the teller id
+     * @param branchid      the branch id
+     * @param depositAmount delta
+     * @return int
+     */
+    public static int depositProcedure(final int accid,
+                                       final int tellerid,
+                                       final int branchid,
+                                       final int depositAmount) {
         String cmmnt = "DEP:" + depositAmount + ";BAL:"
                 + (balanceProcedure(accid) + depositAmount)
                 + ";ACC:" + accid + ";TEL:" + tellerid + ";BRA:"
                 + branchid + ".";
         int newBal = (balanceProcedure(accid) + depositAmount);
         try {
-            CallableStatement stmt = conn.prepareCall("{call depositProcedure(" + accid + ","
-                    + depositAmount + ","
-                    + branchid + ","
-                    + tellerid + ","
-                    + newBal + ",'"
-                    + cmmnt.substring(0, 30)
-                    + "')}");
-            Boolean hasResult = stmt.execute();
+            CallableStatement stmt = conn.prepareCall(
+                    "{call depositProcedure(" + accid + ","
+                            + depositAmount + ","
+                            + branchid + ","
+                            + tellerid + ","
+                            + newBal + ",'"
+                            + cmmnt.substring(0, 30)
+                            + "')}");
+            boolean hasResult = stmt.execute();
             if (hasResult) {
                 ResultSet res = stmt.getResultSet();
-                while (res.next()) {
+                if (res.next()) {
                     return res.getInt("balance");
                 }
             }
@@ -53,13 +75,20 @@ public class StoredProceduresManager {
         return 0;
     }
 
+    /**
+     * Methode to call the balance procedure.
+     *
+     * @param accountid the account id
+     * @return balance of account
+     */
     public static int balanceProcedure(final int accountid) {
         try {
-            CallableStatement stmt = conn.prepareCall("{call balanceProcedure(" + accountid + ")}");
-            Boolean hasResult = stmt.execute();
+            CallableStatement stmt = conn.prepareCall(
+                    "{call balanceProcedure(" + accountid + ")}");
+            boolean hasResult = stmt.execute();
             if (hasResult) {
                 ResultSet res = stmt.getResultSet();
-                while (res.next()) {
+                if (res.next()) {
                     return res.getInt("balance");
                 }
             }
@@ -69,14 +98,20 @@ public class StoredProceduresManager {
         return 0;
     }
 
-
+    /**
+     * Methode to call the analyse procedure.
+     *
+     * @param depositAmount delta
+     * @return number of deposits with this deposit amount.
+     */
     public static int analyseProcedure(final int depositAmount) {
         try {
-            CallableStatement stmt = conn.prepareCall("{call analyseProcedure(" + depositAmount + " )}");
-            Boolean hasResult = stmt.execute();
+            CallableStatement stmt = conn.prepareCall(
+                    "{call analyseProcedure(" + depositAmount + " )}");
+            boolean hasResult = stmt.execute();
             if (hasResult) {
                 ResultSet res = stmt.getResultSet();
-                while (res.next()) {
+                if (res.next()) {
                     return res.getInt("Anz");
                 }
             }
@@ -86,7 +121,10 @@ public class StoredProceduresManager {
         return 0;
     }
 
-    public static void StringsToCreateProcedures() {
+    /**
+     * Methode with sql commands to create Stored Procedures..
+     */
+    public static void stringsToCreateProcedures() {
         String analyse = "CREATE PROCEDURE `analyseProcedure`(IN `depositAmount` INT) COMMENT 'represents the analyse transaction' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT Count(accid) as Anz FROM history WHERE delta = depositAmount GROUP BY delta; END";
         String balance = "CREATE PROCEDURE `balanceProcedure`(IN `accountid` INT) COMMENT 'represents the balance transaction' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT balance FROM accounts WHERE accid = accountid; END";
         String deposit = "CREATE PROCEDURE `depositProcedure`(IN `accountId` INT, IN `depositAmount` INT, IN `braId` INT, IN `telId` INT, IN `newBal` INT, IN `comm` CHAR(30)) COMMENT 'represents deposit transaction' NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN UPDATE accounts SET balance = (SELECT balance FROM accounts WHERE accid = accountId) + depositAmount WHERE accid = accountId; UPDATE branches SET balance = (SELECT balance FROM branches WHERE branchid = braId) + depositAmount WHERE branchid = braId; UPDATE tellers SET balance = (SELECT balance FROM tellers WHERE tellerid = telId) + depositAmount WHERE tellerid = telId; INSERT INTO history (accid, tellerid, delta, branchid, accbalance, history.cmmnt) VALUES (accountId,telId,depositAmount,braId,newBal,comm); END";
