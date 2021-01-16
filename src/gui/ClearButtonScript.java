011eb55e0107eadd0a552c3005c18907b5e9e19f
Erik <erik@bigdon.de>
2021-01-16 13:23:03 -0800

    Clear Button
    working clear button

--- src/gui/ClearButtonScript.java
+++ src/gui/ClearButtonScript.java
@@ -1,11 +1,14 @@
 package gui;
 
+import program.TableManager;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClearButtonScript implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        new TableManager();
+        TableManager.clearHistory();
     }
 }
